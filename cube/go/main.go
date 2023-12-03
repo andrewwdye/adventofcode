69ba6a9ffd328d4ab486cf5f06e678e22ac689dc
Andrew Dye <andrewwdye@gmail.com>
2023-12-03 11:05:55 -0800

    [cube] step 2

--- cube/go/main.go
+++ cube/go/main.go
@@ -71,8 +71,56 @@ func validLine(line string, r, g, b int) (int, error) {
 	return strconv.Atoi(game[1])
 }
 
+func sumPowerFile(file string) (int, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return 0, err
+	}
+
+	return sumPower(f)
+}
+
+func sumPower(r io.Reader) (int, error) {
+	s := bufio.NewScanner(r)
+	sum := 0
+	for s.Scan() {
+		power, err := power(s.Text())
+		if err != nil {
+			return 0, err
+		}
+		sum += power
+	}
+
+	fmt.Printf("%d\n", sum)
+	return sum, nil
+}
+
+func power(line string) (int, error) {
+	red_re := regexp.MustCompile(`(\d+) red`)
+	green_re := regexp.MustCompile(`(\d+) green`)
+	blue_re := regexp.MustCompile(`(\d+) blue`)
+
+	result := 1
+	for _, re := range []*regexp.Regexp{red_re, green_re, blue_re} {
+		matches := re.FindAllStringSubmatch(line, -1)
+		max := 0
+		for _, m := range matches {
+			count, err := strconv.Atoi(m[1])
+			if err != nil {
+				return 0, err
+			}
+			if count > max {
+				max = count
+			}
+		}
+		result *= max
+	}
+	return result, nil
+}
+
 func main() {
-	_, err := countValidFile(os.Args[1], 12, 13, 14)
+	// _, err := countValidFile(os.Args[1], 12, 13, 14)
+	_, err := sumPowerFile(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- cube/go/main_test.go
+++ cube/go/main_test.go
@@ -31,3 +31,11 @@ func TestValidLine(t *testing.T) {
 	assert.Equal(t, 5, lo.Must(validLine("Game 5: 1 red, 1 green, 1 blue", 2, 2, 2)))
 	assert.Equal(t, 0, lo.Must(validLine("Game 5: 1 red, 1 green, 1 blue", 0, 0, 0)))
 }
+
+func TestPower(t *testing.T) {
+	assert.Equal(t, 48, lo.Must(power("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")))
+	assert.Equal(t, 12, lo.Must(power("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")))
+	assert.Equal(t, 1560, lo.Must(power("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")))
+	assert.Equal(t, 630, lo.Must(power("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red")))
+	assert.Equal(t, 36, lo.Must(power("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")))
+}
