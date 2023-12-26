76b7766dea3e28ce755e9b4eded30671da33212b
Andrew Dye <andrewwdye@gmail.com>
2023-12-26 10:05:03 -0800

    14 reflector dishes part 2

--- 14-reflector-dish/go/pkg/dish.go
+++ 14-reflector-dish/go/pkg/dish.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"bufio"
 	"io"
+	"slices"
 )
 
 func Solve1(reader io.Reader) (int, error) {
@@ -27,7 +28,44 @@ func Solve2(reader io.Reader) (int, error) {
 	return weight(lines), nil
 }
 
+func encodeLines(lines []string) string {
+	result := ""
+	for _, line := range lines {
+		result += line
+	}
+	return result
+}
+
+func copyLines(lines []string) []string {
+	out := make([]string, 0, len(lines))
+	return append(out, lines...)
+}
+
 func spin(lines []string, times int) []string {
+	cache := map[string][]string{}
+	firstSeen := map[string]int{}
+	// w := make([]int, 100)
+	for i := 0; i < times; i += 1 {
+		// w = append(w[1:100], weight(lines))
+		// fmt.Println(i, w)
+		start := encodeLines(lines)
+		if first, ok := firstSeen[start]; ok {
+			period := i - first
+			if (times-first)%period == 0 {
+				return lines
+			}
+		}
+		if spinOut, ok := cache[start]; ok {
+			lines = copyLines(spinOut)
+			continue
+		}
+		lines = tiltUp(lines)
+		lines = tiltLeft(lines)
+		lines = tiltDown(lines)
+		lines = tiltRight(lines)
+		cache[start] = copyLines(lines)
+		firstSeen[start] = i
+	}
 	return lines
 }
 
@@ -60,6 +98,52 @@ func tiltUp(lines []string) []string {
 			}
 		}
 
+	}
+	return lines
+}
+
+func tiltDown(lines []string) []string {
+	slices.Reverse(lines)
+	tiltUp(lines)
+	slices.Reverse(lines)
+	return lines
+}
+
+func tiltLeft(lines []string) []string {
+	for i := 0; i < len(lines); i += 1 {
+		for j, next := 0, 0; j < len(lines[i]); j += 1 {
+			switch lines[i][j] {
+			case '.':
+				_ = 0
+			case 'O':
+				if next != j {
+					lines[i] = lines[i][:next] + "O" + lines[i][next+1:]
+					lines[i] = lines[i][:j] + "." + lines[i][j+1:]
+				}
+				next += 1
+			case '#':
+				next = j + 1
+			}
+		}
+	}
+	return lines
+}
+
+func reverseString(s string) string {
+	result := ""
+	for i := len(s) - 1; i >= 0; i -= 1 {
+		result += string(s[i])
+	}
+	return result
+}
+
+func tiltRight(lines []string) []string {
+	for i := 0; i < len(lines); i += 1 {
+		lines[i] = reverseString(lines[i])
+	}
+	tiltLeft(lines)
+	for i := 0; i < len(lines); i += 1 {
+		lines[i] = reverseString(lines[i])
 	}
 	return lines
 }
--- 14-reflector-dish/go/pkg/dish_test.go
+++ 14-reflector-dish/go/pkg/dish_test.go
@@ -35,6 +35,41 @@ O..#.OO...
 	assert.Equal(t, strings.Split(expected, "\n"), result, strings.Join(result, "\n"))
 }
 
+func TestSpin(t *testing.T) {
+	input := `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+
+	assert.Equal(t, strings.Split(`.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....`, "\n"), spin(strings.Split(input, "\n"), 1))
+
+	assert.Equal(t, strings.Split(`.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O`, "\n"), spin(strings.Split(input, "\n"), 2))
+}
+
 func TestSolve1(t *testing.T) {
 	input := `O....#....
 O.OO#....#
@@ -48,4 +83,19 @@ O.#..O.#.#
 #OO..#....`
 
 	assert.Equal(t, 136, lo.Must(Solve1(strings.NewReader(input))))
+}
+
+func TestSolve2(t *testing.T) {
+	input := `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+
+	assert.Equal(t, 64, lo.Must(Solve2(strings.NewReader(input))))
 }
