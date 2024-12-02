f3149d0621072afc13f5c9a6f4aea6bc75ed1165
Andrew Dye <andrewwdye@gmail.com>
2024-12-01 22:44:59 -0800

    Day 2, part 1

---
+++ 2024/src/bin/02-red-nosed/main.rs
@@ -0,0 +1,66 @@
+use clap::Parser;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, value_parser = clap::value_parser!(u8).range(1..=2), default_value_t = 1)]
+    part: u8,
+
+    input: String
+}
+
+fn main() -> Result<(), std::io::Error>{
+    let cli = Cli::parse();
+    let result = match cli.part {
+        1 => solve1(&mut BufReader::new(File::open(cli.input)?))?,
+        2 => solve2(&mut BufReader::new(File::open(cli.input)?))?,
+        _ => unreachable!(),
+    };
+    print!("{result}");
+    Ok(())
+}
+
+fn solve1<R: BufRead>(input: &mut R) -> Result<i32, std::io::Error>{
+    let mut safe = 0; 
+    for line in input.lines() {
+        let levels = line?
+            .split(' ')
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+        let mut diffs = levels.iter().zip(levels.iter().skip(1)).map(|(&x, &y)| y - x);
+        let increasing = diffs.clone().next().unwrap() > 0;
+        if diffs.all(|x| {
+            x.abs() >= 1 && x.abs() <= 3 &&
+            if increasing {
+                x > 0
+            } else {
+                x < 0
+            }
+        }) {
+            safe += 1;
+        }
+    }
+    Ok(safe)
+}
+
+fn solve2<R: BufRead>(_: &mut R) -> Result<i32, std::io::Error>{
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!(solve1(&mut BufReader::new(input.as_bytes())).unwrap(), 2);
+    }
+}
