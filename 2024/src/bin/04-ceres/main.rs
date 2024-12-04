5ff1e30a396a2fc78cc23b9e2b28bd47a180b728
Andrew Dye <andrewwdye@gmail.com>
2024-12-03 22:48:21 -0800

    Day 4, part 1

---
+++ 2024/src/bin/04-ceres/main.rs
@@ -0,0 +1,76 @@
+use clap::Parser;
+use std::fs::read_to_string;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, value_parser = clap::value_parser!(u8).range(1..=2), default_value_t = 1)]
+    part: u8,
+
+    input: String
+}
+
+fn main() -> Result<(), std::io::Error>{
+    let cli = Cli::parse();
+    let input = read_to_string(cli.input)?;
+    let result = match cli.part {
+        1 => solve1(input.as_str())?,
+        2 => solve2(input.as_str())?,
+        _ => unreachable!(),
+    };
+    print!("{result}");
+    Ok(())
+}
+
+fn solve1(input: &str) -> Result<i32, std::io::Error>{
+    let lines: Vec<&str> = input.lines().collect();
+    let mut count = 0;
+    for i in 0..lines.len() {
+        for j in 0..lines[i].len() {
+            for (di, dj) in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)].iter() {
+                count += search(&lines, i as i32, j as i32, *di, *dj, "XMAS");
+            }
+        }
+    }
+    Ok(count)
+}
+
+fn search(lines: &Vec<&str>, i: i32, j: i32, di: i32, dj: i32, word: &str) -> i32 {
+    if word.len() == 0 {
+        return 1;
+    }
+    if i < 0 || i >= lines.len() as i32 {
+        return 0;
+    }
+    if j < 0 || j >= lines[i as usize].len() as i32 {
+        return 0;
+    }
+    if lines[i as usize].as_bytes()[j as usize] != word.as_bytes()[0] {
+        return 0;
+    }
+    search(lines, i + di, j + dj, di, dj, &word[1..])
+}
+
+fn solve2(_: &str) -> Result<i32, std::io::Error>{
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!(solve1(input).unwrap(), 18);
+    }
+}
