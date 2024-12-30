eff85a514ce2576c1dbf91225f2e8722aeb75f04
Andrew Dye <andrewwdye@gmail.com>
2024-12-29 22:09:23 -0800

    Day 16, part 2

--- 2024/src/bin/day16.rs
+++ 2024/src/bin/day16.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
 use core::cmp::Reverse;
-use std::{collections::BinaryHeap, collections::HashSet, fs::read_to_string};
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    fmt::Debug,
+    fs::read_to_string,
+};
 
 #[derive(Parser)]
 struct Cli {
@@ -24,16 +28,19 @@ fn main() -> Result<(), std::io::Error> {
 
 fn solve1(input: &str) -> Result<i32, std::io::Error> {
     let m = Maze::new(input);
-    Ok(m.solve())
+    let (cost, _) = m.solve();
+    Ok(cost)
 }
 
-fn solve2(_: &str) -> Result<i32, std::io::Error> {
-    unimplemented!()
+fn solve2(input: &str) -> Result<i32, std::io::Error> {
+    let m = Maze::new(input);
+    let (_, best_path_tiles) = m.solve();
+    Ok(best_path_tiles as i32)
 }
 
 type Location = (i32, i32);
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 enum Direction {
     North = 0,
     East,
@@ -75,6 +82,18 @@ struct Entry {
     location: Location,
     direction: Direction,
     cost: i32,
+    path: HashSet<Location>,
+}
+
+impl Debug for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Entry")
+            .field("location", &self.location)
+            .field("direction", &self.direction)
+            .field("cost", &self.cost)
+            .field("path", &self.path.len())
+            .finish()
+    }
 }
 
 impl Ord for Entry {
@@ -101,24 +120,41 @@ impl Maze {
         Self { grid, start }
     }
 
-    fn solve(&self) -> i32 {
-        let mut visited = HashSet::new();
+    fn solve(&self) -> (i32, usize) {
+        let mut visited = HashMap::new();
         let mut h: BinaryHeap<Reverse<Entry>> = BinaryHeap::new();
+        let mut p = HashSet::new();
+        p.insert(self.start);
         h.push(Reverse(Entry {
             location: self.start,
             direction: Direction::East,
             cost: 0,
+            path: p,
         }));
+        let mut min_cost = i32::MAX;
+        let mut best_path_tiles = HashSet::new();
         loop {
             let entry = h.pop().unwrap().0;
+            if entry.cost > min_cost {
+                return (min_cost, best_path_tiles.len());
+            }
             let element = self.grid[entry.location.1 as usize][entry.location.0 as usize];
             if element == 'E' {
-                return entry.cost;
-            }
-            if element == '#' || visited.contains(&entry.location) {
+                min_cost = entry.cost;
+                for loc in entry.path.iter() {
+                    best_path_tiles.insert(loc.clone());
+                }
                 continue;
             }
-            visited.insert(entry.location);
+            if element == '#' {
+                continue;
+            }
+            if let Some(&prev_cost) = visited.get(&(entry.location, entry.direction)) {
+                if entry.cost > prev_cost {
+                    continue;
+                }
+            }
+            visited.insert((entry.location, entry.direction), entry.cost);
             for (dir, turn_cost) in [
                 (entry.direction, 0),
                 (entry.direction.left(), 1000),
@@ -126,11 +162,18 @@ impl Maze {
             ] {
                 let (dx, dy) = dir.steps();
                 let next = (entry.location.0 + dx, entry.location.1 + dy);
-                h.push(Reverse(Entry {
+                if entry.path.contains(&next) {
+                    continue;
+                }
+                let mut path = entry.path.clone();
+                path.insert(next);
+                let e = Entry {
                     location: next,
                     direction: dir,
                     cost: entry.cost + turn_cost + 1,
-                }));
+                    path: path,
+                };
+                h.push(Reverse(e));
             }
         }
     }
@@ -178,5 +221,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(solve1(SAMPLE_INPUT1).unwrap(), 7036);
         assert_eq!(solve1(SAMPLE_INPUT2).unwrap(), 11048);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve2(SAMPLE_INPUT1).unwrap(), 45);
+        assert_eq!(solve2(SAMPLE_INPUT2).unwrap(), 64);
     }
 }
